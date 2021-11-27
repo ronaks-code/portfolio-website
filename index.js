f0bbfc5b535dcd6d54d8cf321bc6f5cac2670895
RosalieWessels <rosalie.wessels@yahoo.com>
2021-11-27 15:26:03 -0800

    Added code

---
+++ index.js
@@ -0,0 +1,46 @@
+var granimInstance = new Granim({
+    element: '#canvas-image-blending',
+    direction: 'top-bottom',
+    isPausedWhenNotInView: true,
+    image : {
+        source: 'assets/snow.jpg',
+        blendingMode: 'multiply',
+    },
+    states : {
+        "default-state": {
+            gradients: [
+                ['#29323c', '#485563'],
+                ['#FF6B6B', '#556270'],
+                ['#80d3fe', '#7ea0c4'],
+                ['#f0ab51', '#eceba3']
+            ],
+            transitionSpeed: 8000
+        }
+    }
+});
+
+//MODAL
+var modal = document.getElementById("modalMijnMedicijn");
+
+// Get the button that opens the modal
+var btn = document.getElementById("modalMijnMedicijnButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
